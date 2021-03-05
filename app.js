eada1c98c9ed0f487da31790be2a1cd0b81cf2d7
Arjun Nayak <arjun430@gmail.com>
2021-03-05 22:56:27 +0530

    mysql testing

--- app.js
+++ app.js
@@ -24,8 +24,11 @@ connection.connect()
 
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
+  
 
   console.log('The solution is: ', rows[0].solution);
+  res.send("Connection success and the solution is = "+ rows[0].solution);
+
 })
 
 connection.end()
