58056d442e8201c97e8078fd1f2d48d5def70454
Arjun Nayak <arjun430@gmail.com>
2021-03-05 22:48:37 +0530

    database testing

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ app.get('/app/user', (req, res) => {
   res.send('Hello User')
 })
 
+app.get('/app/mysqltest',(req,res)=>{
+
+  var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'gilpilco_gilpil'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution);
+})
+
+connection.end()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
