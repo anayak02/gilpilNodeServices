5faa99b0b7f5783ed84a8d2f2a62918a6e17c10c
Arjun Nayak <arjun430@gmail.com>
2021-03-06 14:28:25 +0530

    added user module

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = express()
 const port = process.env.PORT || 3000;
 const mysqldbConn = require('./config/mysql.db.config')
 
+/**
+ *   For security check need to add below line Access-Control-Allow-Origin
+ *   it tell that only the give URL are allow to access the resource
+ *   server can be really strict, and specify that only one origin can access it
+ * */
+app.use((req,res,next)=>{
+  
+  const corsWhitelist = [
+    'http://localhost:4200',
+    'https://www.gilpil.com',
+    'http://www.gilpil.com',
+    ];
+    console.log("Request URL Comming from : ",req.headers.origin);
+    console.log(">>",corsWhitelist.indexOf(req.headers.origin))
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+   
+    next();
+})
+
 app.get('/app', (req, res) => {
   res.send('Welcome to gilpil.com')
 })
@@ -20,13 +42,14 @@ app.get('/app/mysqltest',(req,res)=>{
            
     })
 
-app.get("/app/getAllUserDetails",(req,res)=>{
+app.get("/app/user/getAllUserDetails",(req,res)=>{
+          
           mysqldbConn.query('select * from user',(err,rows)=>{
 
             if(err){
               req.fresh('error',err);
-             }else{
-              res.send({data:rows});
+         }else{
+              res.send({rows:rows});
 
             }
     })
