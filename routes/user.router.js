60d2f70c073db92815c689226fa2bede620a9ab4
Arjun Nayak <arjun430@gmail.com>
2021-03-13 23:49:38 +0530

    write the login service code

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const chalk = require('chalk');
 var mysqldbConn = require('../config/mysql.db.config');
+const { Color } = require('chalk');
 
 router.get('/findAllUsers',function(req,res,next){
 
@@ -12,8 +14,52 @@ router.post('/b', function(req, res,next) {
      res.send("emaiid \t="+req.body.email+"\t password \t ="+req.body.password);
 });
 
+router.post('/signin',(req,res)=>{
+    console.log(chalk.bold.green("email = ",req.body.email));
+    console.log(chalk.bold.green("password = ",req.body.password));
 
-router.post('/signup',(req,res,next)=>{
+    let sql = "select * from user u where u.login_user_id=? AND u.password = ?";
+    let values=[
+        [req.body.email],
+        [req.body.password]
+    ];
+
+    try{
+
+        mysqldbConn.query(sql,[values[0],values[1]],(err,result)=>{
+
+            if(err){
+                res.send({reusult:"error:"+err.message});
+                
+            }else{
+                let row=[];
+                row = result;
+               console.log("result length "+row.length) ;
+               if(row.length===0){
+                   res.status(400).send({result:"failed",user:null});
+               }else{
+
+                    if(req.body.email===result[0].email && req.body.password === result[0].password){
+                        res.status(200).send({result:"success",user:result});       
+                    }else{
+                        res.status(400).send({result:"failed",user:null});   
+                    }
+                    
+               }
+               
+            }
+
+        })
+
+    }catch(e){
+        console.log(chalk.red("Error"+e));
+        res.status(400).send("Erro : "+e.message)
+    }
+    
+    
+    
+})
+router.post('/signup',(req,res)=>{
 
     console.log(req.body.email);
     console.log(req.body.password);
@@ -50,7 +96,7 @@ router.post('/signup',(req,res,next)=>{
   })
   
 router.get("/getAllUserDetails",(req,res,next)=>{
-     
+     console.log(chalk.blue("Get All User Details is called !!"));
         try{
            mysqldbConn.query('select * from user',(err,rows)=>{
                if(err){
